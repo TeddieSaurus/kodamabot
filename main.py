23eff10deed166d8f06cd8eba138e21bc19b2b96
TeddieSaurus <dusanv3@gmail.com>
2022-05-02 22:05:55 +0200

    Updated with Add fact

--- main.py
+++ main.py
@@ -5,18 +5,17 @@ import discord
 my_secret = os.environ['TOKEN']
 
 
-
 journal_link = 'https://docs.google.com/document/d/174f4fFTwMTyGx-PiXQkqHwnFD_XFHyGkJMGAisLy_MI/edit'
 takeshi_journal_link = 'https://docs.google.com/document/d/1kOTNzkU0G5joaz-TQK8V8623Qk3pQ9zTdNicgMKQNGs/edit'
 reference_link = 'https://docs.google.com/document/d/1KE4VNmGDQJ3zAJiXoZuOS08734y73rDb6NEb-77sRyY/edit'
-facts = ["The land of Ginru is lush and vast!",
-         "Jin kinda smells like wet dog",
-         "Ryuujin are said to watch over travelers",
-         "The church of the Snail claims the world is a big snail",
-         "Buying something abundant in one town and selling it where its rare is sure to net a profit",
-         "You'd need to eat around 400 bananas for them to kill you with potassium poisoning",
-         "Moru is technologically advanced, they even have self moving carriages!",
-         "Sometimes the journey is better than the destination"]
+
+global facts
+
+f = open("facts.txt", "r")
+facts = f.readlines()
+f.close()
+
+
 client = discord.Client()
 
 
@@ -27,18 +26,30 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    global facts
+    msg_content = message.content.lower()
+    print(message.author)
+    print(message.author)
     if message.author == client.user:
         return
-    if message.content.startswith('!Kodama hello'):
+    if msg_content.startswith('!kodama hello'.lower()):
         await message.channel.send('Hello {}!'.format(message.author.name))
-    if message.content.startswith('!Kodama journal'):
+    if msg_content.startswith('!kodama journal'.lower()):
         await message.channel.send(f'Hiya the link to the journal is here {journal_link}')
-    if message.content.startswith('!Kodama Takeshi journal'):
+    if msg_content.startswith('!kodama takeshi journal'.lower()):
         await message.channel.send(f"Hiya here is the journal of Takeshi {takeshi_journal_link}")
-    if message.content.startswith('!Kodama reference'):
+    if msg_content.startswith('!kodama reference'.lower()):
         await message.channel.send(f"Hiya here is the reference sheet for Ryuutama {reference_link}")
-    if message.content.startswith("!Kodama fact"):
+    if msg_content.startswith("!kodama fact".lower()):
         await message.channel.send(facts[random.randint(0, len(facts)-1)])
+    if msg_content.startswith("!kodama add fact".lower()):
+        with open("facts.txt", "a") as file:
+            print(msg_content)
+            file.write(msg_content[16:] + f" --Author: {message.author.name}" + "\n")
+        with open("facts.txt", "r") as file:
+            facts = file.readlines()
+        await message.channel.send("Fact added!")
+
 
 keepalive()
 client.run(my_secret)
